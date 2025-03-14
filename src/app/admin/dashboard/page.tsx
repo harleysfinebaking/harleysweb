669361a15a78624d1e5d29b3dc22ecec4d47b1f8
harleysfinebaking <digital@harleys.com>
2025-03-14 23:11:04 +0530

    remove panel data

--- src/app/admin/dashboard/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import withAuth from "@/components/withAuth";
-
-export default withAuth(function DashboardPage() {
-  return <h1 className="text-2xl font-bold">Welcome to the Dashboard</h1>;
-});
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -19,20 +18,4 @@ const app =
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-const firebaseConfigNew = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY_NEW,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN_NEW,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID_NEW,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_NEW,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID_NEW,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID_NEW,
-};
-
-const appNew =
-  getApps().find((app) => app.name === "SECOND_PROJECT") ||
-  initializeApp(firebaseConfigNew, "SECOND_PROJECT");
-const dbNew = getFirestore(appNew);
-const storageNew = getStorage(appNew);
-const authNew = getAuth(appNew);
-
-export { db, storage, dbNew, storageNew, authNew };
+export { db, storage };
